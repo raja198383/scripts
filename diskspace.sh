e051f9c361868246fc3130dd9138d6e1720f9389
raja <raja.techsupport@gmail.com>
2020-01-15 11:00:40 +0530

    diskspace monitor

---
+++ diskspace.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Monitoring disk space utilization and send email alert 
+
+THRESHOULD=14
+mailto=raja
+HOSTNAME=$(hostname)
+
+for path in `/bin/df -h | grep -vE 'Filesystem|tmpfs' | awk '{print $5}' | sed 's/%//g'`
+do 
+      if [ $path -ge $THRESHOULD ]; then
+      df -h | grep $path% >> /tmp/temp
+      fi 
+done
